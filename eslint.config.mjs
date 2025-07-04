1e2c3ae50af2e3470f0d7aff9e3eb10fe5391a99
Grod56 <rodneygaryx29@gmail.com>
2025-07-04 17:21:00 +0200

    Initial fixes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,70 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import pluginNext from "@next/eslint-plugin-next";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
-
-export default eslintConfig;
+export default defineConfig([
+	{
+		files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+		plugins: { js },
+		extends: ["js/recommended"],
+	},
+	{
+		settings: {
+			react: {
+				version: "detect",
+			},
+		},
+	},
+	{
+		files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+		languageOptions: { globals: globals.browser },
+	},
+	{ languageOptions: { globals: globals.jest } },
+	tseslint.configs.recommended,
+	pluginReact.configs.flat.recommended,
+	pluginReact.configs.flat["jsx-runtime"],
+	{
+		plugins: {
+			"@next/next": pluginNext,
+		},
+		files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+		rules: {
+			...pluginNext.configs.recommended.rules,
+			...pluginNext.configs["core-web-vitals"].rules,
+		},
+	},
+	{
+		plugins: {
+			js,
+		},
+		rules: {
+			"no-empty-pattern": "warn",
+		},
+	},
+	{
+		plugins: {
+			"react-hooks": reactHooks,
+		},
+		rules: {
+			...reactHooks.configs.recommended.rules,
+			"react-hooks/rules-of-hooks": "error",
+			"react-hooks/exhaustive-deps": "warn",
+		},
+	},
+	{
+		rules: {
+			"@typescript-eslint/no-empty-object-type": "warn",
+			"@typescript-eslint/no-unused-vars": [
+				"warn",
+				{ argsIgnorePattern: "^_" },
+			],
+			"react/no-unescaped-entities": "warn",
+		},
+	},
+	eslintConfigPrettier,
+]);
