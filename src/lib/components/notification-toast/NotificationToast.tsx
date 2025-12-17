416dde25b64c6ac07a072cf9ccd90bf3bcef41b4
Grod56 <rodneygaryx29@gmail.com>
2025-12-17 06:05:26 +0200

    minor tweaks

--- src/lib/components/notification-toast/NotificationToast.tsx
+++ src/lib/components/notification-toast/NotificationToast.tsx
@@ -43,34 +43,36 @@ const NotificationToast = function ({ model }) {
 	]);
 
 	return (
-		<ToastContainer
-			position={"bottom-center"}
-			className="p-4 position-fixed"
-			style={{ zIndex: 10 }}
-		>
-			<Toast
-				bg={getToastBg()}
-				show={open}
-				onClose={() =>
-					interact({
-						type: "CLOSE",
-					})
-				}
-				onExited={() => {
-					interact({
-						type: "CLEAR_NOTIFICATION",
-					});
-				}}
-				delay={5000}
-				autohide
+		notification?.text && (
+			<ToastContainer
+				position={"bottom-center"}
+				className="p-4 position-fixed"
+				style={{ zIndex: 10 }}
 			>
-				<Toast.Body className="text-center">
-					<span className="text-amber-50 text-base">
-						{notification?.text}
-					</span>
-				</Toast.Body>
-			</Toast>
-		</ToastContainer>
+				<Toast
+					bg={getToastBg()}
+					show={open}
+					onClose={() =>
+						interact({
+							type: "CLOSE",
+						})
+					}
+					onExited={() => {
+						interact({
+							type: "CLEAR_NOTIFICATION",
+						});
+					}}
+					delay={5000}
+					autohide
+				>
+					<Toast.Body className="text-center">
+						<span className="text-amber-50 text-base">
+							{notification.text}
+						</span>
+					</Toast.Body>
+				</Toast>
+			</ToastContainer>
+		)
 	);
 } as ModeledVoidComponent<NotificationToastModel>;
 
